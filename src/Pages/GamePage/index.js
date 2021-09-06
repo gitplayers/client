4b77cfb7cede214c1196b9b560449ed23be7dc10
Zak Gogi <zakgogi1@gmail.com>
2021-09-06 10:28:23 +0100

    Add elements to initial game page and hide them on game start

--- src/Pages/GamePage/index.js
+++ src/Pages/GamePage/index.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Game } from '../../Components';
-
-
+import './style.css';
 
 const GamePage = () => {
 
@@ -10,17 +9,33 @@ const GamePage = () => {
     const startGame = (e) => {
         console.log(e.target)
         e.target.style.display = "none";
+        if (e.target.id === "brideButton"){
+            e.target.nextElementSibling.style.display = "none";
+            let currentElement = e.target.previousElementSibling;
+            for (let i = 0; i < 3; i++){
+                currentElement = currentElement.previousElementSibling;
+                currentElement.style.display = "none";
+            }
+        } else {
+            e.target.previousElementSibling.style.display = "none";
+            let currentElement = e.target.previousElementSibling.previousElementSibling;
+            for (let i = 0; i < 3; i++){
+                currentElement = currentElement.previousElementSibling;
+                currentElement.style.display = "none";
+            }
+        }
+        
         setGameStarted(true)
     }
 
-
-
     return ( 
         <main>
-            <h1>Game</h1>
-            <button onClick={startGame}>start game</button>
-            {gameStarted ? <Game /> : <h2>Waiting for game to start...</h2>}
-            
+            <h1>Welcome!</h1>
+            <h2>How to play...</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam volutpat purus sit amet ipsum varius, at varius lacus tempor. Sed semper mi et nibh suscipit, at consectetur lacus tristique. Suspendisse tempor posuere augue. Vestibulum lobortis dolor libero, in congue turpis tempus ac. Nullam pretium nisi dui, quis viverra sapien ullamcorper quis. Pellentesque sit amet arcu congue, vulputate nisl at, dictum lacus. In vitae velit turpis. Etiam rutrum nunc ac sem lacinia accumsan. Cras laoreet magna dui, in rhoncus orci pulvinar ac. Aliquam erat volutpat. Donec luctus nibh efficitur odio varius, vitae porttitor velit venenatis.</p>
+            {gameStarted ? <Game /> : <h2>Choose a button below to start...</h2>}
+            <button id="brideButton" onClick={startGame}>I'm part of the bridal party!</button>
+            <button id="groomButton" onClick={startGame}>I'm part of the groom party!</button>
         </main> 
     );
 }
