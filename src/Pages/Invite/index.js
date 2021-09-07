f2870fb0e8dadd9ac692780ca1d1966e0f52b30e
Zak Gogi <zakgogi1@gmail.com>
2021-09-07 11:42:44 +0100

    Invite renders title and message

--- src/Pages/Invite/index.js
+++ src/Pages/Invite/index.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useWedding } from "../../Context/WeddingContext";
 
 const Invite = () => {
 
-    return ( <h1>Invite</h1> );
+    const { wedding_name } = useParams();
+    const { weddingData, weddingFetch } = useWedding();
+    const [ invitationData, setInvitationData ] = useState([]);
+    const [ loading, setLoading ] = useState(true);
+
+    useEffect(() => {
+        const getInvitationData = async () => {
+            if (Object.keys(weddingData).length === 0){
+                await weddingFetch(wedding_name);
+            } else {
+                console.log(weddingData.invitation);
+                setInvitationData(weddingData.invitation);
+            }
+        }
+        getInvitationData();
+        setLoading(false);
+    }, [weddingData])
+
+    return (
+        <>
+            {loading ? <h1>Loading..</h1> : 
+            <div>
+                <h1>Invite</h1>
+                <h2>{invitationData.title}</h2>
+                <p>{invitationData.message}</p> 
+            </div>
+            }
+        </>
+    );
 }
  
 export default Invite;
