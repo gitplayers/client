69beab1ca0f936a19be1266e209a657c364c000c
Zak Gogi <zakgogi1@gmail.com>
2021-09-08 17:25:54 +0100

    Home page renders character select

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,7 +1,126 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./style.css";
-import { images } from '../../Helpers';
+import { images, largeSpriteImages } from '../../Helpers';
 const Home = () => {
+
+    const [ currentHair, setCurrentHair ] = useState('H1');
+    const [ currentSkin, setCurrentSkin ] = useState('S1'); 
+    const [ currentDress, setCurrentDress ] = useState('D1');
+    const [ currentEyes, setCurrentEyes ] = useState('E1');
+
+    const cycleOptions = (direction, bodyPart) => {
+        let partList;
+        let initial = bodyPart.slice(0, 1);
+        if (initial === 'D'){
+            if (currentSkin.slice(1) <= 3){
+                partList = ['D1', 'D2', 'D3', 'D4'];
+            } else {
+                partList = ['D5', 'D6', 'D7'];
+            }
+        } else {
+            if (currentSkin.slice(1) <= 3){
+                partList = [`${initial}1`, `${initial}2`, `${initial}3`]
+            } else {
+                partList = [`${initial}4`, `${initial}5`, `${initial}6`]
+            }
+        }
+        let currentIndex;
+        switch(bodyPart){
+            case 'Hair':
+                currentIndex = partList.indexOf(currentHair);
+                break;
+            case 'Skin':
+                currentIndex = partList.indexOf(currentSkin);
+                break;
+            case 'Dress':
+                currentIndex = partList.indexOf(currentDress);
+                break;
+            case 'Eyes':
+                currentIndex = partList.indexOf(currentEyes);
+                break;
+            default:
+                return;
+        }
+        let indexToChoose;
+        if (direction === "next"){
+            if (currentIndex + 1 > partList.length - 1){
+                indexToChoose = 0;
+            } else {
+                indexToChoose = currentIndex + 1
+            }
+        } else {
+            if (currentIndex - 1 < 0){
+                indexToChoose = partList.length - 1
+            } else {
+                indexToChoose = currentIndex - 1
+            }
+        }
+        switch(bodyPart){
+            case 'Hair':
+                setCurrentHair(partList[indexToChoose]);
+                break;
+            case 'Skin':
+                setCurrentSkin(partList[indexToChoose]);
+                break;
+            case 'Dress':
+                setCurrentDress(partList[indexToChoose]);
+                break;
+            case 'Eyes':
+                setCurrentEyes(partList[indexToChoose]);
+                break;
+            default:
+                return;
+        }
+    }   
+
+    const renderButtons = (direction) => {
+        let symbol;
+        if (direction === "next"){
+            symbol = ">"
+        } else {
+            symbol = "<"
+        }
+        let buttonList = ['Hair', 'Eyes', 'Skin', 'Dress']
+        return buttonList.map(b => {
+            return (
+                <section key={b} className="buttonDiv">
+                    <button id={`${b}NextButton`} onClick={() => cycleOptions(direction, b)}>{symbol}</button>
+                </section>
+            )
+        })
+    }
+
+    const renderCharacterSelect = () => {
+        return (
+            <div id="characterSelect">
+                <img className="hair" src={largeSpriteImages[`${currentHair}.png`].default}></img>
+                <img className="skin" src={largeSpriteImages[`${currentSkin}.png`].default}></img>
+                <img className="dress" src={largeSpriteImages[`${currentDress}.png`].default}></img>
+                <img className="eyes" src={largeSpriteImages[`${currentEyes}.png`].default}></img>
+                <div id="nextButtons">
+                    {renderButtons('next')}
+                </div>
+                <div id="prevButtons">
+                    {renderButtons('prev')}
+                </div>
+            </div>
+        )
+    }
+
+    const toggleGender = () => {
+        if (currentHair.slice(1) <= 3){
+            setCurrentHair('H4');
+            setCurrentSkin('S4');
+            setCurrentDress('D5');
+            setCurrentEyes('E4');
+        } else {
+            setCurrentHair('H1');
+            setCurrentSkin('S1');
+            setCurrentDress('D1');
+            setCurrentEyes('E4');
+        }
+    }
+
     return (
         <>
             <div>
@@ -30,6 +149,8 @@ const Home = () => {
             </div>
             <div>
                 <h2>Checkout some of our character customization</h2>
+                <button id="genderToggle" onClick={toggleGender}>Toggle character style</button>
+                {renderCharacterSelect()}
             </div>
         </>
     );
